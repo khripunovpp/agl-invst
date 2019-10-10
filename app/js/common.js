b992e4ac7e67c7bc0c6fd838f005eee79f60e337
Павел Хрипунов <khripunovpp@gmail.com>
2019-10-10 15:53:12 +0300

    add sections: header, hero, howItWorks

---
+++ app/js/common.js
@@ -0,0 +1,60 @@
+var Util = {
+    randomInteger: function(min, max) {
+        var rand = min + Math.random() * (max - min)
+        rand = Math.round(rand);
+        return rand;
+    },
+    scrollToEl: function(el, offset) {
+        $("html,body").animate({ scrollTop: el.offset().top + (offset || 0) }, 500);
+    },
+    trimString: function(string) {
+        return string.split(' ').join('');
+    }
+}
+
+$(function() {
+
+    $('[data-scroll-to]').on('click', function(event) {
+        event.preventDefault();
+        var to = $(this).attr('data-scroll-to')
+        Util.scrollToEl($(to))
+    });
+
+    var header = $('.header')
+
+    $('.hamburger').on('click', function(event) {
+        event.preventDefault();
+        _toggle()
+    });
+
+    function _toggle() {
+        header.toggleClass('opened')
+        $("body").toggleClass('fixed')
+    }
+
+    function _default() {
+        header.removeClass('opened')
+        $("body").removeClass('fixed')
+    }
+
+    $('.header__nav a').on('click', function(event) {
+        event.preventDefault();
+        var to = $(this).attr('href')
+        Util.scrollToEl($(to))
+        if ($(window).width() < 991) {
+            _toggle()
+        }
+    });
+
+    
+    $(window).on('resize', function() {
+        if ($(window).width() > 991) {
+            _default()
+        } else {
+
+            $('.stages__item').removeAttr('style')
+        }
+    });
+
+
+});
